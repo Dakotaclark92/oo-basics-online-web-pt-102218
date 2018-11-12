a4479459d12593fdb769f197474b21f9abfa56a2
Dakota Clark <bannaray95@gmail.com>
2018-11-12 02:48:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  def initialize(condition)
+  def initialize(brand, condition)
+    @brand =  brand
     @condition = condition
   end
   
